9dc29850ce915b2b0c8cf06e8645fa0e2c671928
Monim Hossain <flynmonim@gmail.com>
2022-09-27 03:33:13 +0600

    image upload add on edit option

--- src/pages/EditProduct.jsx
+++ src/pages/EditProduct.jsx
@@ -1,19 +1,12 @@
 import React, { useEffect, useState } from "react";
-import {
-  Button,
-  Divider,
-  Form,
-  Input,
-  Select,
-  Upload,
-  Modal,
-} from "antd";
+import { Button, Divider, Form, Input, Select, Upload, Modal } from "antd";
 import { Col, Row } from "react-bootstrap";
 import "antd/dist/antd.css";
 import "../css/Common.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TopBar from "../components/TopBar";
-import { getApi, postApi } from "../api/commonApi";
+import { getApi, postApi, postApiWithoutContentType } from "../api/commonApi";
+import { UploadOutlined } from "@ant-design/icons";
 import { useParams } from "react-router";
 
 const { Option } = Select;
@@ -25,6 +18,7 @@ export default function EditProduct() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [categoryName, setCategoryName] = useState("");
   const [description, setDescription] = useState("");
+  const [file, setFile] = useState("");
   const { id } = useParams();
 
   useEffect(() => {
@@ -71,35 +65,55 @@ export default function EditProduct() {
     }
   };
 
-  const deductUndefined = (values) => {
-    if(values.name === undefined){
+  const deductUndefinedAndAppend = (values) => {
+    const data = new FormData();
+    data.append('id', id);
+
+    if (values.name === undefined) {
       delete values.name;
+    }else{
+      data.append('name', values.name);
     }
-    if(values.category_id === undefined){
+
+    if (values.category_id === undefined) {
       delete values.category_id;
+    }else{
+      data.append('category_id', values.category_id);
     }
-    if(values.sku === undefined){
+
+    if (values.sku === undefined) {
       delete values.sku;
+    }else{
+      data.append('sku', values.sku);
     }
-    if(values.status === undefined){
+
+    if (values.status === undefined) {
       delete values.status;
+    }else{
+      data.append('status', values.status);
     }
-    if(values.price === undefined){
+
+    if (values.price === undefined) {
       delete values.price;
+    }else{
+      data.append('price', values.price);
     }
-    return values;
-  }
+
+    if (description) {
+      data.append('description', description);
+    }
+
+    if(file){
+      data.append('image_url', file);
+    }
+
+    return data;
+  };
 
   const onFinish = async (values) => {
-    values = deductUndefined(values);
+    const data = deductUndefinedAndAppend(values)
 
-    if(description){
-      values.description = description;
-    }
-
-    values.id = id;
-
-    const response = await postApi("/api/product/update", values);
+    const response = await postApiWithoutContentType("/api/product/update", data);
     if (response.error) {
       for (let key in response.error) {
         alert(response.error[key]);
@@ -109,6 +123,10 @@ export default function EditProduct() {
       window.location.href = "/";
     }
   };
+
+  function handleFileChange(e) {
+    setFile(e.file.originFileObj)
+}
 
   return (
     <>
@@ -134,7 +152,10 @@ export default function EditProduct() {
                   </Form.Item>
 
                   <Form.Item label="Category" name="category_id">
-                    <Select defaultValue={product.category_id} placeholder="Select a category">
+                    <Select
+                      defaultValue={product.category_id}
+                      placeholder="Select a category"
+                    >
                       {categories &&
                         categories.map((category) => (
                           <Option value={category.id}>{category.name}</Option>
@@ -145,7 +166,11 @@ export default function EditProduct() {
                   <br />
 
                   <Form.Item label="SKU" name="sku">
-                    <Input defaultValue={product.sku} name="sku" placeholder="SKU" />
+                    <Input
+                      defaultValue={product.sku}
+                      name="sku"
+                      placeholder="SKU"
+                    />
                   </Form.Item>
 
                   <Form.Item label="Description">
@@ -154,7 +179,7 @@ export default function EditProduct() {
                       placeholder="Description"
                       name="description"
                       defaultValue={product.description}
-                      onChange={(e)=>setDescription(e.target.value)}
+                      onChange={(e) => setDescription(e.target.value)}
                     />
                   </Form.Item>
 
@@ -174,17 +199,19 @@ export default function EditProduct() {
                     </Select>
                   </Form.Item>
 
-                  {/* <Form.Item label="Image">
-                <Upload
-                  accept=".png, .jpg, .jpeg"
-                  //   action={`${process.env.REACT_APP_BASE_URL}/api/courses/test-upload`}
-                  beforeUpload={(file) => {
-                    return true;
-                  }}
-                >
-                  <Button icon={<UploadOutlined />}>Click to Upload</Button>
-                </Upload>
-              </Form.Item> */}
+                  <Form.Item label="Image">
+                    <Upload
+                      accept=".png, .jpg, .jpeg"
+                      action={`${process.env.REACT_APP_BASE_URL}/api/test-upload`}
+                      beforeUpload={(file) => {
+                        return true;
+                      }}
+                      name="image_url"
+                      onChange={(info) => handleFileChange(info)}
+                    >
+                      <Button icon={<UploadOutlined />}>Click to Upload</Button>
+                    </Upload>
+                  </Form.Item>
 
                   <Button type="primary" htmlType="submit">
                     Submit
