948ade67a5ac26886aa67af418a6161e5367e6ea
Monim Hossain <flynmonim@gmail.com>
2022-09-27 18:56:55 +0600

    responsive login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,7 @@ import "antd/dist/antd.css";
 import "../css/Common.css";
 import { Link } from "react-router-dom";
 
-export default function Login() {  
+export default function Login() {
   const onFinish = async (values) => {
     const data = {
       client_id: process.env.REACT_APP_CLIENT_ID,
@@ -20,68 +20,73 @@ export default function Login() {
 
     const response = await postApi("/oauth/token", data);
 
-    if(response.error){
-        alert(response.message);
-    }else{
-        localStorage.setItem('access_token', JSON.stringify(response.access_token))
-        localStorage.setItem('email', JSON.stringify(values.username))
-        window.location.href = '/'
+    if (response.error) {
+      alert(response.message);
+    } else {
+      localStorage.setItem(
+        "access_token",
+        JSON.stringify(response.access_token)
+      );
+      localStorage.setItem("email", JSON.stringify(values.username));
+      window.location.href = "/";
     }
   };
 
   return (
-    <div className="center">
-      <h1>Product Management Software</h1>
-      <Form
-        name="normal_login"
-        className="login-form"
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: "Please input your Username!",
-            },
-          ]}
+    <div className="login-container">
+      <div className="center">
+        <h1>Product Management Software</h1>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
         >
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Username"
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your Password!",
-            },
-          ]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Username!",
+              },
+            ]}
           >
-            Log in
-          </Button>
-          <br />
-          <br /> Not a member? <Link to="/registration"> register now!</Link>
-        </Form.Item>
-      </Form>
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Password!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Log in
+            </Button>
+            <br />
+            <br /> Not a member? <Link to="/registration"> register now!</Link>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 }
